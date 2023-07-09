dbb7c801dc4bf150e7f33554e6faf9116e6460ce
Philipasdf <ng.philip.91@gmail.com>
2023-07-09 08:46:42 +0200

    simple button for triggering a notification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { Observable, map } from 'rxjs';
-
-interface Pokemon {
-  id: number;
-  name: string;
-}
 
 @Component({
   selector: 'app-root',
@@ -13,32 +7,11 @@ interface Pokemon {
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  apiUrl = 'https://pokeapi.co/api/v2/pokemon/';
+  notyText = 'any notification text';
 
-  $data: Observable<Pokemon>;
-
-  formInput: number;
-
-  constructor(private http: HttpClient) {
-    // initialize this.formInput with a random number between 1 and 1010
-    this.formInput = Math.floor(Math.random() * 1010) + 1;
-
-    this.$data = this.fetchData();
-  }
+  constructor(private http: HttpClient) {}
 
   onFormSubmit() {
-    this.$data = this.fetchData();
-  }
-
-  fetchData(): Observable<Pokemon> {
-    return this.http.get(this.apiUrl + this.formInput).pipe(
-      map((data: any) => {
-        console.log(data);
-        return {
-          id: data.id,
-          name: data.name,
-        };
-      })
-    );
+    console.log(this.notyText);
   }
 }
